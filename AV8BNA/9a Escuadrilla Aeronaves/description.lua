2d38780694bd6dc5929f03f90bfe56ad8dc549a7
buxidy <lnovak1993@gmail.com>
2020-10-31 16:15:26 +0100

    Added AV8B 9a Escuadrilla

---
+++ AV8BNA/9a Escuadrilla Aeronaves/description.lua
@@ -0,0 +1,69 @@
+-- Razbam AV-8B NA Harrier II 
+-- 9ª Escuadrilla Aeronaves - Armada Española
+-- Skin made by PorcoRosso86 
+
+livery = {
+
+	{"AV8B1", 0 ,"AV8_T1_ARMADA",false};
+	{"AV8B1", ROUGHNESS_METALLIC ,"AV8_T1_ARMADA_ROUGHMET",false};
+	{"AV8B2", 0 ,"AV8_T2_ARMADA",false};
+	{"AV8B2", ROUGHNESS_METALLIC ,"AV8_T2_ARMADA_ROUGHMET",false};
+	{"AV8B3", 0 ,"AV8_T3_ARMADA",false};
+	{"AV8B3", ROUGHNESS_METALLIC ,"AV8_T3_ARMADA_ROUGHMET",false};
+	{"AV8B4", 0 ,"AV8_T4_ARMADA",false};
+--	{"AV8B7", 0 ,"AV8_T7_ARMADA",false};
+	{"AV8B8", 0 ,"AV8_T8_ARMADA",false};
+	{"AV8B_WV", 0 ,"AV8_T4_ARMADA",false};
+	{"Pilot_AV8B_gear", 0 ,"Pilot_AV8B_gear_ARMADA",false};
+	{"pilot_AV8B_body", 0 ,"pilot_AV8B_ARMADA",false};
+	
+	{"AV8B_T1_FUSE1", 0 ,"AV8_T1_ARMADA",false};
+	{"AV8B_T1_FUSE1", ROUGHNESS_METALLIC ,"AV8_T1_ARMADA_ROUGHMET",false};
+	{"AV8B_T1_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T1_FUSE10", 0 ,"AV8_T1_ARMADA",false};
+	{"AV8B_T1_FUSE10", ROUGHNESS_METALLIC ,"AV8_T1_ARMADA_ROUGHMET",false};
+	{"AV8B_T1_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T2_FUSE1", 0 ,"AV8_T2_ARMADA",false};
+	{"AV8B_T2_FUSE1", ROUGHNESS_METALLIC ,"AV8_T2_ARMADA_ROUGHMET",false};
+	{"AV8B_T2_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T2_FUSE10", 0 ,"AV8_T2_ARMADA",false};
+	{"AV8B_T2_FUSE10", ROUGHNESS_METALLIC ,"AV8_T2_ARMADA_ROUGHMET",false};
+	{"AV8B_T2_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_FUSE1", 0 ,"AV8_T3_ARMADA",false};
+	{"AV8B_T3_FUSE1", ROUGHNESS_METALLIC ,"AV8_T3_ARMADA_ROUGHMET",false};
+	{"AV8B_T3_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T3_FUSE10", 0 ,"AV8_T3_ARMADA",false};
+	{"AV8B_T3_FUSE10", ROUGHNESS_METALLIC ,"AV8_T3_ARMADA_ROUGHMET",false};
+	{"AV8B_T3_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_TAIL1", 0 ,"AV8_T3_ARMADA",false};
+	{"AV8B_T3_TAIL1", ROUGHNESS_METALLIC ,"AV8_T3_ARMADA_ROUGHMET",false};	
+	{"AV8B_T3_TAIL1", DECAL ,"empty",true};
+	
+	{"AV8B_T3_TAIL10", 0 ,"AV8_T3_ARMADA",false};
+	{"AV8B_T3_TAIL10", ROUGHNESS_METALLIC ,"AV8_T3_ARMADA_ROUGHMET",false};	
+	{"AV8B_T3_TAIL10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_TAIL100", 0 ,"AV8_T3_ARMADA",false};
+	{"AV8B_T3_TAIL100", ROUGHNESS_METALLIC ,"AV8_T3_ARMADA_ROUGHMET",false};	
+	{"AV8B_T3_TAIL100", DECAL ,"empty",true};
+	
+    {"pilot_AV8B_patch", 0 ,"pilot_AV8B_patch_ARMADA",false};
+	
+	{"AV8B4", ROUGHNESS_METALLIC ,"AV8_T4_RoughMet",true};
+	{"AV8B7", ROUGHNESS_METALLIC ,"AV8_T7_RoughMet",true};
+	{"AV8B8", ROUGHNESS_METALLIC ,"AV8_T8_RoughMet",true};
+	{"pilot_AV8B_helmet", ROUGHNESS_METALLIC ,"Pilot_AV8B_hgu68_RoughMet",true};
+	{"pilot_AV8B_body", ROUGHNESS_METALLIC ,"pilot_AV8B_RoughMet",true};
+
+
+}
+
+name = "9a Escuadrilla Aeronaves - Armada Española"
+
+
