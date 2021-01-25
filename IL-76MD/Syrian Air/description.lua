6f08462cd8afdbe746e7dac3344339497ca96961
Vishari <47961951+Vishari@users.noreply.github.com>
2021-01-25 03:14:45 -0500

    Removed old AI Skins
    Removed obsolete story mission skins from the pack.

--- IL-76MD/Syrian Air/description.lua
+++
@@ -1,8 +0,0 @@
-livery = {
-	{"IL76-MAP-01", 0,"IL76MD-SKIN01-01",false};
-	{"IL76-MAP-02", 0,"IL76MD-SKIN01-02",false};
-	{"76MD-SKIN01-02", 0,"IL76MD-SKIN01-02",false};
-	{"76MD-SKIN01-05", 0,"empty.tga",true};
-}
-name = "Syrian Air"
-countries = {"RUS","SYR"}
