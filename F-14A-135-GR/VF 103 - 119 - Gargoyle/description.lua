d1b9c9f69baea1f5d52b9996b84a8c9b9d7466b8
Vishari <47961951+Vishari@users.noreply.github.com>
2021-01-27 18:47:44 -0500

    VF-103 A-model compatibility with latest update

--- F-14A-135-GR/VF 103 - 119 - Gargoyle/description.lua
+++ F-14A-135-GR/VF 103 - 119 - Gargoyle/description.lua
@@ -3,11 +3,11 @@ livery = {
 
 --Diffuse  -----------------------------------
 
-{"HB_F14_EXT_01", 0 ,"../../f-14b/VF 103 - Airboss/HB_F14_EXT_01",false};
-{"HB_F14_EXT_02", 0 ,"../../f-14b/VF 103 - Airboss/HB_F14_EXT_02",false};
+{"HB_F14_EXT_01", 0 ,"../../f-14b/VF 103 - 119 - Gargoyle/HB_F14_EXT_01",false};
+{"HB_F14_EXT_02", 0 ,"../../f-14b/VF 103 - 119 - Gargoyle/HB_F14_EXT_02",false};
 {"HB_F14_EXT_03", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_EXT_03",false};
 {"HB_F14_EXT_04", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_EXT_04",false};
-{"HB_F14_EXT_TAIL", 0 ,"../../f-14b/VF 103 - Airboss/HB_F14_EXT_TAIL",false};
+{"HB_F14_EXT_TAIL", 0 ,"../../f-14b/VF 103 - 119 - Gargoyle/HB_F14_EXT_TAIL",false};
 {"HB_F14_WING_LEFT_01", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_WING_LEFT",false};
 {"HB_F14_WING_RIGHT", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_WING_RIGHT",false};
 {"HB_F14_LOD1_3in1", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_LOD1_3in1",false};
@@ -23,6 +23,6 @@ livery = {
 
 
 }
-name = "VF-103 - Airboss"
+name = "VF-103 - 119 - Gargoyle"
 
 --countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
