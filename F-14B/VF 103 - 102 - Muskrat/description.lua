382e1aa4701c6eb40b32281f5c7f51d10cf354cc
Muskrat85 <77038592+Muskrat85@users.noreply.github.com>
2021-03-18 23:30:31 -0700

    Muskrat B and A LUA fix

--- F-14B/VF 103 - 102 - Muskrat/description.lua
+++ F-14B/VF 103 - 102 - Muskrat/description.lua
@@ -10,7 +10,7 @@ livery = {
 {"HB_F14_EXT_TAIL", 0 ,"HB_F14_EXT_TAIL",false};
 {"HB_F14_WING_LEFT_01", 0 ,"../VF 103 - JTF-13/HB_F14_WING_LEFT",false};
 {"HB_F14_WING_RIGHT", 0 ,"../VF 103 - JTF-13/HB_F14_WING_RIGHT",false};
-{"HB_F14_LOD1_3in1", 0 ,"HB_F14_LOD1_3in1",false};
+{"HB_F14_LOD1_3in1", 0 ,"../VF 103 - JTF-13/HB_F14_LOD1_3in1",false};
 {"HB_F14_LOD3_3in1", 0 ,"../VF 103 - JTF-13/HB_F14_LOD3_3in1",false};
 {"HB_F14_EXT_PILOT_HELMET", 0 ,"../VF 103 - JTF-13/Helmets/Custom/JTF_F14_HELMET_MUSKRAT",false};
 {"HB_F14_EXT_PILOT_HELMET", 1 ,"../VF 103 - JTF-13/Helmets/Line/JTF_F14_HELMET_NORMAL_MAP",false};
@@ -21,6 +21,6 @@ livery = {
 {"HB_F14_EXT_DROPTANKS", 0 ,"../VF 103 - JTF-13/HB_F14_EXT_DROPTANK",false};
 
 }
-name = "VF-103 - 102 - Muskrat"
+name = "VF-103 - 103 - Muskrat"
 
 --countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
