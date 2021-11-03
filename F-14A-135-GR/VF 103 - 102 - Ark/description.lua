7995a1c53e24bb07d89e8cfd14d50654bd7af2b1
null <null@0x36a.com>
2021-11-02 18:02:51 -0700

    VF-103 - Moving Ark to 102

--- F-14A-135-GR/VF 103 - 102 - Ark/description.lua
+++ F-14A-135-GR/VF 103 - 102 - Ark/description.lua
@@ -3,13 +3,13 @@ livery = {
 
 --Diffuse  -----------------------------------
 
-{"HB_F14_EXT_01", 0 ,"../../f-14b/VF 103 - 109 - Ark/HB_F14_EXT_01",false};
-{"HB_F14_EXT_02", 0 ,"../../f-14b/VF 103 - 109 - Ark/HB_F14_EXT_02",false};
+{"HB_F14_EXT_01", 0 ,"../../f-14b/VF 103 - 102 - Ark/HB_F14_EXT_01",false};
+{"HB_F14_EXT_02", 0 ,"../../f-14b/VF 103 - 102 - Ark/HB_F14_EXT_02",false};
 {"HB_F14_EXT_03", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_EXT_03",false};
 {"HB_F14_EXT_04", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_EXT_04",false};
-{"HB_F14_EXT_TAIL", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_EXT_TAIL",false};
-{"HB_F14_WING_LEFT_01", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_WING_LEFT",false};
-{"HB_F14_WING_RIGHT", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_WING_RIGHT",false};
+{"HB_F14_EXT_TAIL", 0 ,"../../f-14b/VF 103 - 102 - Ark/HB_F14_EXT_TAIL",false};
+{"HB_F14_WING_LEFT_01", 0 ,"../../f-14b/VF 103 - 102 - Ark/HB_F14_WING_LEFT",false};
+{"HB_F14_WING_RIGHT", 0 ,"../../f-14b/VF 103 - 102 - Ark/HB_F14_WING_RIGHT",false};
 {"HB_F14_LOD1_3in1", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_LOD1_3in1",false};
 {"HB_F14_LOD3_3in1", 0 ,"../../f-14b/VF 103 - JTF-13/HB_F14_LOD3_3in1",false};
 {"HB_F14_EXT_PILOT_HELMET", 0 ,"../../f-14b/VF 103 - JTF-13/Helmets/Custom/JTF_F14_HELMET_ARK",false};
